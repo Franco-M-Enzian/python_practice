d5a42e72d1e951b0506e93660ac900b003acf189
Franco-M-Enzian <emf0000b@gmail.com>
2025-04-21 16:21:36 +0900

    part3

--- file_manipulator.py
+++ file_manipulator.py
@@ -1,8 +1,48 @@
-pathname = 'test.txt'
+import sys
+
+cmd = sys.argv[1]
+inputpath = sys.argv[2]
 contents = ''
 
-with open(pathname) as f:
-    contents = f.read()
+if cmd == 'reverse':
+    outputpath = sys.argv[3]
+
+    with open(inputpath) as f:
+        contents = f.read()
+
+    with open(outputpath, 'w') as f:
+        f.write(contents[::-1])
+
+if cmd == 'copy':
+    outputpath = sys.argv[3]
+
+    with open(inputpath) as f:
+        contents = f.read()
+
+    with open(outputpath, 'w') as f:
+        f.write(contents)
+        
+if cmd == 'depulicate-contents':
+    n = sys.argv[3]
+    n = int(n)
+
+    with open(inputpath) as f:
+        contents = f.read()
+
+    with open(inputpath, 'w') as f:
+        while n > 0:
+            f.write(contents)
+            n -= 1
+
+if cmd == 'replace-string':
+    needle = sys.argv[3]
+    new_string = sys.argv[4]
     
-with open(pathname, 'w') as f:
-    f.write(contents + "\nHEY!")
+    if needle != 'needle' or new_string != 'newstring':
+        print("Try again.")
+    
+    with open(inputpath) as f:
+        contents = f.read()
+
+    with open(inputpath, 'w') as f:
+        f.write(contents.replace(needle, new_string))
